b80688f01f76ed3b110804b495a3f89ae8ee197b
YangBioinformatics <shipingyang@cau.edu.cn>
2024-10-15 19:33:46 +0800

    update

--- README.md
+++
@@ -1,3 +0,0 @@
-# Maize-Pan-genome
-Detailed bioinformatics scripts and methods used in the Maize-Pan-genome paper.
-Contributors: Shiping Yang, Yijie Wang
---
+++ sv-identification/src/long_reads_mapping.sh
@@ -0,0 +1,10 @@
+# Taken CIMBL70 (NanoPore) or CIMBL55 (PacBio) as an example
+# sample 30× fastq data, -p represents the proportion which determined by the original total size of sequencing data; 0.3 represents 30%
+seqkit sample -s100 CIMBL70.filt.fa.gz -p 0.3 -2|/software/gzip-1.10/gzip -c - >30x_CIMBL70.fastq.gz
+# long reads mapping, for NanoPore data, such as CIMBL70
+minimap2 -ax map-ont --MD -t 32 /data/genome/B73v5/genome.fa CIMBL70.fq.gz >CIMBL70.sam
+# long reads mapping, for PacBio data, such as CIMBL55
+minimap2 -ax map-pb --MD -t 32 /data/genome/B73v5/genome.fa CIMBL55.fq.gz >CIMBL55.sam
+samtools view -@  32 -Sb  CIMBL70.sam | samtools sort -@ 32 -o CIMBL70.bam -
+samtools index -@ 32 CIMBL70.bam
+rm CIMBL70.sam
